a349d1f6e47675b34161b7ee60ab8c642f09c0b4
francisco.santos <>
2024-01-31 01:17:49 +0100

    feat: selection sort algorithm

---
+++ selection-sort.js
@@ -0,0 +1,26 @@
+function selectionSort(array) {
+    const n = array.length;
+    const arr = structuredClone(array)
+
+    for (let i = 0; i < n - 1; i++) {
+        // Encontra o índice do menor elemento na parte não ordenada
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Troca o menor elemento encontrado com o primeiro elemento não ordenado
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+
+    return arr;
+}
+
+// Exemplo de uso
+const unsortedArray = [38, 27, 43, 3, 9, 82, 10];
+const sortedArray = selectionSort(unsortedArray);
+
+console.log("Array não ordenado:", unsortedArray);
+console.log("Array ordenado:", sortedArray);
