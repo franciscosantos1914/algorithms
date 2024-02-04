b9431b9ce0741601ed6ece92facc70be7ababb33
francisco.santos <>
2024-02-05 00:48:33 +0100

    feat: binary search algorithm

---
+++ binary-search.js
@@ -0,0 +1,16 @@
+function binarySearch(array, target) {
+    let start = 0;
+    let end = array.length - 1;
+
+    while (start <= end) {
+        const mid = Math.floor((start + end) / 2);
+
+        if (array[mid] === target) return mid;
+        else if (array[mid] < target) start = mid + 1;
+        else end = mid - 1;
+    }
+
+    return -1;
+}
+
+console.log(binarySearch([1, 3, 5, 7, 9, 11], 7));
